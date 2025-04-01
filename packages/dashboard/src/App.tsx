5bd82a2e03018a2bd3a6f5cd3b66751d566ca36b
Balu Babu <balub997@gmail.com>
2025-04-02 02:38:27 +0530

    chore: added loom video demo for vader

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
@@ -31,14 +30,7 @@ const App = () => (
             </PublicRoute>
           }
         />
-        <Route
-          path="/landing"
-          element={
-            <PublicRoute>
-              <Landing />
-            </PublicRoute>
-          }
-        />
+
         <Route
           path="/auth"
           element={
